f6a185f87c9ae65091f8d9ffeea95899db929a13
mohaelmi <moha.abdi1771@gmail.com>
2023-11-13 11:23:36 -0600

    create-react-app and update README

---
+++ front_end/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Magic Project for the final starts here!! </h1>
+    </div>
+  );
+}
+
+export default App;
