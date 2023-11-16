4288fc27ce395faec14890481988a3733044ed80
Onyinye Ochi <Osemene.go@gmail.com>
2023-11-15 22:57:58 -0500

    Delete unused files and update dependencies

--- frontend/src/components/NewTask.js
+++ frontend/src/components/NewTask.js
@@ -1,10 +1,8 @@
-import React from 'react'
-import "../styles/Newtask.css";
+import React from 'react';
+import '../styles/Newtask.css';
 
 function NewTask() {
-  return (
-  <h1>new task</h1>
-  )
+  return <h1>new task</h1>;
 }
 
-export default NewTask
+export default NewTask;
