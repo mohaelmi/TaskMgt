5323ea8e45b678e5b01bbe7b4c9bb24d9c29cc3e
Onyinye Ochi <Osemene.go@gmail.com>
2023-11-15 22:24:13 -0500

    resolve conflict and refactor auth routes

--- backend/server.js
+++ backend/server.js
@@ -3,68 +3,41 @@
 //const tasksQueries = require("./db/queries/tasks");
 // Web server config
 // const sassMiddleware = require("./lib/sass-middleware");
-const cookieSession = require("cookie-session");
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
+const cookieSession = require('cookie-session');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const taskRoutes = require('./routes/taskRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 // The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.json());
 app.use(
   cookieSession({
-    name: "session",
-    keys: ["key1"],
+    name: 'session',
+    keys: ['key1'],
   })
 );
 
-const taskRoutes = require("./routes/taskRoutes");
-
-
 //api/tasks endpoint
-app.use("/api/tasks", taskRoutes);
+app.use('/api/tasks', taskRoutes);
+app.use('/api/auth', authRoutes);
 
 // /api/users endpoint
 //app.use("/users", userRoutes);
 
-
-
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-
-
-
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});const express = require('express');
-const bodyParser = require('body-parser');
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.post('/register', (req, res) => {
-  console.log(req.body);
-
-  // implement register logic
-  return res.status(200).json(req.body);
-});
-
-app.post('/login', (req, res) => {
-  console.log(req.body);
-
-  // implement login logic
-  return res.status(200).json(req.body);
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from server!' });
 });
 
 app.listen(PORT, () => {
