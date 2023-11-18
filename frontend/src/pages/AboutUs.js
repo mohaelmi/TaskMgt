3e5d4beadc5110b30ac93a522a79a9d5d39d94a6
Onyinye Ochi <Osemene.go@gmail.com>
2023-11-18 00:29:35 -0500

    Remove unused imports and fix component paths

---
+++ frontend/src/pages/AboutUs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <h2>About Us</h2>
+      <p>About Us</p>
+      <p>About Us</p>
+      <p>About Us</p>
+      <p>About Us</p>
+      <p>About Us</p>
+    </div>
+  );
+};
+export default AboutUs;
